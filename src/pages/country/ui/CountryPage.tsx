a786ce347ddf80cd670a04a45cb5b8729aed2f70
tarasovvvv <steelcoat@mail.ru>
2025-02-18 00:09:36 +0400

    back button fixes

--- src/pages/country/ui/CountryPage.tsx
+++ src/pages/country/ui/CountryPage.tsx
@@ -9,11 +9,14 @@ function CountryPage() {
   const navigate = useNavigate();
   return (
     <>
-      <button onClick={() => navigate(-1)} className={styles.backButton}>
-        <svg fill="currentColor" width="2em" height="2em" viewBox="0 0 52 52" xmlns="http://www.w3.org/2000/svg">
-          <path d="M50,24H6.83L27.41,3.41a2,2,0,0,0,0-2.82,2,2,0,0,0-2.82,0l-24,24a1.79,1.79,0,0,0-.25.31A1.19,1.19,0,0,0,.25,25c0,.07-.07.13-.1.2l-.06.2a.84.84,0,0,0,0,.17,2,2,0,0,0,0,.78.84.84,0,0,0,0,.17l.06.2c0,.07.07.13.1.2a1.19,1.19,0,0,0,.09.15,1.79,1.79,0,0,0,.25.31l24,24a2,2,0,1,0,2.82-2.82L6.83,28H50a2,2,0,0,0,0-4Z" />
-        </svg>
-      </button>
+      <div className={styles.back}>
+        <button onClick={() => navigate(-1)} className={styles.backButton}>
+          <svg className={styles.backImg} fill="currentColor"  viewBox="0 0 52 52" xmlns="http://www.w3.org/2000/svg">
+            <path d="M50,24H6.83L27.41,3.41a2,2,0,0,0,0-2.82,2,2,0,0,0-2.82,0l-24,24a1.79,1.79,0,0,0-.25.31A1.19,1.19,0,0,0,.25,25c0,.07-.07.13-.1.2l-.06.2a.84.84,0,0,0,0,.17,2,2,0,0,0,0,.78.84.84,0,0,0,0,.17l.06.2c0,.07.07.13.1.2a1.19,1.19,0,0,0,.09.15,1.79,1.79,0,0,0,.25.31l24,24a2,2,0,1,0,2.82-2.82L6.83,28H50a2,2,0,0,0,0-4Z" />
+          </svg>
+        </button>
+        <p className={styles.backTitle}>Назад</p>
+      </div>
       <Loader isOpen={isLoading} />
       <Errorer isOpen={error !== undefined} error={error && "data" in error ? `${country} - ${error.data?.message}` : error?.message} />
       <img className={styles.img} src={countryData?.flags.svg} alt={countryData?.flags.alt} />
